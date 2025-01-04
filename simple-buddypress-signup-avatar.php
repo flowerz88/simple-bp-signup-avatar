94fc2931df87c12170ea872261a168b8dae64607
Butterfly88 <aless@live.nl>
2025-01-04 12:47:42 +0100

    Update and rename simple-bp-signup-avatar.php to simple-buddypress-signup-avatar.php

--- simple-buddypress-signup-avatar.php
+++ simple-buddypress-signup-avatar.php
@@ -1,14 +1,14 @@
 <?php
 /*
 Plugin Name: Simple BuddyPress Signup Avatar
-Plugin URI: https://github.com/flowerz88/simple-bp-signup-avatar
+Plugin URI: https://github.com/flowerz88/simple-buddypress-signup-avatar
 Description: Enhances BuddyPress registration by allowing users to upload avatars during sign-up.
 Version: 1.0
 Author: Butterfly88
 Author URI: https://github.com/flowerz88
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
-Text Domain: simple-bp-signup-avatar
+Text Domain: simple-buddypress-signup-avatar
 Domain Path: /languages
 
 Simple BuddyPress Signup Avatar is free software: you can redistribute it and/or modify
@@ -21,13 +21,13 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU General Public License for more details.
 */
-
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 // Load the main functionality file
 require_once __DIR__ . '/bp_avatar_registration.php';
 require_once __DIR__ . '/admin-settings.php';
 
 // Load the text domain for translations
-function custom_bp_avatar_load_textdomain() {
-    load_plugin_textdomain('simple-bp-signup-avatar', false, dirname(plugin_basename(__FILE__)) . '/languages');
+function sbsa_load_textdomain() {
+    load_plugin_textdomain('simple-buddypress-signup-avatar', false, dirname(plugin_basename(__FILE__)) . '/languages');
 }
-add_action('plugins_loaded', 'custom_bp_avatar_load_textdomain');
+add_action('plugins_loaded', 'sbsa_load_textdomain');
